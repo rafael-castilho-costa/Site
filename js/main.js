c90033c466e81134e6425437516980fbf72e06a8
Rafael Castilho <rafael@tecnologia.blue>
2023-01-25 15:03:57 -0300

    update

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ function showSlides(n) {
    
 }*/
 
-let slideIndex = 0;
+/* let slideIndex = 0;
 showSlides();
 
 function showSlides() {
@@ -38,4 +38,4 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+} */
